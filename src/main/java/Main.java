9526974d0dd2770b7785a133322dd1160aa0a922
onleetosh <tashi.thompson2@gmail.com>
2025-08-04 13:00:48 -0400

    create game window

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import view.Window;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Window window = Window.get();
+        //window.run();
+
+        Window.get().run();
+
+
+
+    }
+}
